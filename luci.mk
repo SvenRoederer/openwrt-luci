825ea036eb43ca66fa2e8d830c30a8310cb29de6
Sven Roederer <SvenRoederer@users.noreply.github.com>
2019-03-03 22:19:22 +0100

    luci.mk: remove private definition
    keep all definitions, that might be interesting for other feeds too.
    LuCI-private definitions are in feed.mk

--- luci.mk
+++ luci.mk
@@ -4,13 +4,6 @@
 # This is free software, licensed under the Apache License, Version 2.0 .
 #
 
-LUCI_NAME?=$(notdir ${CURDIR})
-LUCI_TYPE?=$(word 2,$(subst -, ,$(LUCI_NAME)))
-LUCI_BASENAME?=$(patsubst luci-$(LUCI_TYPE)-%,%,$(LUCI_NAME))
-LUCI_LANGUAGES:=$(sort $(filter-out templates,$(notdir $(wildcard ${CURDIR}/po/*))))
-LUCI_DEFAULTS:=$(notdir $(wildcard ${CURDIR}/root/etc/uci-defaults/*))
-LUCI_PKGARCH?=$(if $(realpath src/Makefile),,all)
-
 # Language code titles
 LUCI_LANG.ca=Català (Catalan)
 LUCI_LANG.cs=Čeština (Czech)
@@ -48,8 +41,6 @@ LUCI_MENU.proto=5. Protocols
 LUCI_MENU.lib=6. Libraries
 
 
-PKG_NAME?=$(LUCI_NAME)
-
 PKG_VERSION?=$(if $(DUMP),x,$(strip $(shell \
 	if svn info >/dev/null 2>/dev/null; then \
 		revision="svn-r$$(LC_ALL=C svn info | sed -ne 's/^Revision: //p')"; \
@@ -81,54 +72,9 @@ PKG_GITBRANCH?=$(if $(DUMP),x,$(strip $(shell \
 	echo "$$variant" \
 )))
 
-PKG_RELEASE?=1
-PKG_INSTALL:=$(if $(realpath src/Makefile),1)
-PKG_BUILD_DEPENDS += lua/host luci-base/host LUCI_CSSTIDY:csstidy/host $(LUCI_BUILD_DEPENDS)
-PKG_CONFIG_DEPENDS += CONFIG_LUCI_SRCDIET CONFIG_LUCI_JSMIN CONFIG_LUCI_CSSTIDY
-
 PKG_BUILD_DIR:=$(BUILD_DIR)/$(PKG_NAME)
 
 include $(INCLUDE_DIR)/package.mk
-
-define Package/$(PKG_NAME)
-  SECTION:=luci
-  CATEGORY:=LuCI
-  SUBMENU:=$(if $(LUCI_MENU.$(LUCI_TYPE)),$(LUCI_MENU.$(LUCI_TYPE)),$(LUCI_MENU.app))
-  TITLE:=$(if $(LUCI_TITLE),$(LUCI_TITLE),LuCI $(LUCI_NAME) $(LUCI_TYPE))
-  DEPENDS:=$(LUCI_DEPENDS)
-  $(if $(LUCI_EXTRA_DEPENDS),EXTRA_DEPENDS:=$(LUCI_EXTRA_DEPENDS))
-  $(if $(LUCI_PKGARCH),PKGARCH:=$(LUCI_PKGARCH))
-endef
-
-ifneq ($(LUCI_DESCRIPTION),)
- define Package/$(PKG_NAME)/description
-   $(strip $(LUCI_DESCRIPTION))
- endef
-endif
-
-# Language selection for luci-base
-ifeq ($(PKG_NAME),luci-base)
- define Package/luci-base/config
-   config LUCI_SRCDIET
-	bool "Minify Lua sources"
-	default n
-
-   config LUCI_JSMIN
-	bool "Minify JavaScript sources"
-	default y
-
-   config LUCI_CSSTIDY
-        bool "Minify CSS files"
-        default y
-
-   menu "Translations"$(foreach lang,$(LUCI_LANGUAGES),
-
-     config LUCI_LANG_$(lang)
-	   tristate "$(shell echo '$(LUCI_LANG.$(lang))' | sed -e 's/^.* (\(.*\))$$/\1/') ($(lang))")
-
-   endmenu
- endef
-endif
 
 define Build/Prepare
 	for d in luasrc htdocs root src; do \
@@ -139,21 +85,6 @@ define Build/Prepare
 	done
 	$(call Build/Prepare/Default)
 endef
-
-define Build/Configure
-endef
-
-ifneq ($(wildcard ${CURDIR}/src/Makefile),)
- MAKE_PATH := src/
- MAKE_VARS += FPIC="$(FPIC)" LUCI_VERSION="$(PKG_VERSION)" LUCI_GITBRANCH="$(PKG_GITBRANCH)"
-
- define Build/Compile
-	$(call Build/Compile/Default,clean compile)
- endef
-else
- define Build/Compile
- endef
-endif
 
 HTDOCS = /www
 LUA_LIBRARYDIR = /usr/lib/lua
@@ -211,54 +142,3 @@ define Package/$(PKG_NAME)/install
 	  $(CP) $(PKG_INSTALL_DIR)/* $(1)/; \
 	else true; fi
 endef
-
-ifneq ($(LUCI_DEFAULTS),)
-define Package/$(PKG_NAME)/postinst
-[ -n "$${IPKG_INSTROOT}" ] || {$(foreach script,$(LUCI_DEFAULTS),
-	(. /etc/uci-defaults/$(script)) && rm -f /etc/uci-defaults/$(script))
-	exit 0
-}
-endef
-endif
-
-
-LUCI_BUILD_PACKAGES := $(PKG_NAME)
-
-define LuciTranslation
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)
-    SECTION:=luci
-    CATEGORY:=LuCI
-    TITLE:=$(PKG_NAME) - $(1) translation
-    HIDDEN:=1
-    DEFAULT:=LUCI_LANG_$(1)||(ALL&&m)
-    DEPENDS:=$(PKG_NAME)
-    PKGARCH:=all
-  endef
-
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)/description
-    Translation for $(PKG_NAME) - $(LUCI_LANG.$(1))
-  endef
-
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)/install
-	$$(INSTALL_DIR) $$(1)/etc/uci-defaults
-	echo "uci set luci.languages.$(subst -,_,$(1))='$(LUCI_LANG.$(1))'; uci commit luci" \
-		> $$(1)/etc/uci-defaults/luci-i18n-$(LUCI_BASENAME)-$(1)
-	$$(INSTALL_DIR) $$(1)$(LUCI_LIBRARYDIR)/i18n
-	$(foreach po,$(wildcard ${CURDIR}/po/$(1)/*.po), \
-		po2lmo $(po) \
-			$$(1)$(LUCI_LIBRARYDIR)/i18n/$(basename $(notdir $(po))).$(1).lmo;)
-  endef
-
-  define Package/luci-i18n-$(LUCI_BASENAME)-$(1)/postinst
-	[ -n "$$$${IPKG_INSTROOT}" ] || {
-		(. /etc/uci-defaults/luci-i18n-$(LUCI_BASENAME)-$(1)) && rm -f /etc/uci-defaults/luci-i18n-$(LUCI_BASENAME)-$(1)
-		exit 0
-	}
-  endef
-
-  LUCI_BUILD_PACKAGES += luci-i18n-$(LUCI_BASENAME)-$(1)
-
-endef
-
-$(foreach lang,$(LUCI_LANGUAGES),$(eval $(call LuciTranslation,$(lang))))
-$(foreach pkg,$(LUCI_BUILD_PACKAGES),$(eval $(call BuildPackage,$(pkg))))
